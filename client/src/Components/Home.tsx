b3b5a015bf55101de23ffbe9699ef485e4350e59
wojci <wojciech.kurdybacha@gmail.com>
2021-11-28 03:50:42 +0100

    create basic home page elements

---
+++ client/src/Components/Home.tsx
@@ -0,0 +1,15 @@
+import './Home.css'
+
+export default function Home(){
+    return (
+        <div className='Home'>
+
+            <h1 className='title'>Sync Your cringe PissBaby playlists</h1>
+            <div className='start-button-wrapper'>
+
+                <button className='start-button'>START</button>
+            </div>
+
+        </div>
+    )
+}
