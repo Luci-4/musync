f56baf85db4c409c87b7cf7129b23f38c7d897c1
wojci <wojciech.kurdybacha@gmail.com>
2021-11-29 12:21:39 +0100

    setup backend with express

--- client/src/Components/Home.tsx
+++ client/src/Components/Home.tsx
@@ -1,4 +1,32 @@
 import './Home.css'
+const playlists_url = "https://api.spotify.com/v1/tracks/2KrxsD86ARO5beq7Q0Drfqa";
+
+const fetchRequest = async (url: string) => {
+    console.log("connecting to the cock")
+    let res = await fetch(`http://localhost:3000/${url}`)
+    console.log(res)
+    let data = await res.text()
+    console.log(data)
+    return data
+}
+const getCode = () => {
+    // const code = getJsonFromUrl(window.location.href)
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    
+    const code = urlParams.get('code')
+    return code
+}
+
+const login = async () => {
+    // const url = await fetchRequest("login")
+    // console.log("url is:", url)
+    // window.location.href = url;
+   fetch('/users')
+   .then(res => res.json()) 
+   .then(users => console.log(users))
+    // let res = await fetch(`htt`) 
+}
 
 export default function Home(){
     return (
@@ -7,7 +35,7 @@ export default function Home(){
             <h1 className='title'>Sync Your cringe PissBaby playlists</h1>
             <div className='start-button-wrapper'>
 
-                <button className='start-button'>START</button>
+                <button className='start-button' onClick={login}>START</button>
             </div>
 
         </div>
